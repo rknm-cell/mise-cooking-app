f9ea9487ac26f35d68c103911a0887e85ab7bf64
rknm-cell <racknahm@gmail.com>
2025-06-30 16:17:28 -0400

    set up backend express api connection to supabase

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -1,9 +1,13 @@
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import * as schema from "./schema"
+import * as schema from "./schema";
 
 config({ path: '.env' }); // or .env.local
 
-const client = postgres(process.env.DATABASE_URL!);
+const client = postgres(process.env.DATABASE_URL!, {
+  ssl: 'require',
+  max: 1,
+});
+
 export const db = drizzle(client, { schema } );
