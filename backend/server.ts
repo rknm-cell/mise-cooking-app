9ce4c0d65413e170e2892f2724a2d1e3d493bd6e
rknm-cell <racknahm@gmail.com>
2025-06-30 17:33:53 -0400

    updating port routing

--- backend/server.ts
+++ backend/server.ts
@@ -11,7 +11,7 @@ import * as schema from './db/schema.js';
 config();
 
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(helmet());
